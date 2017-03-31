4d7bce34dd3db9a97965b8953cb17fee0de1ae78
Alejandro Sanchez <a@stcsolutions.com.ve>
2017-03-31 01:32:58 -0400

    urdecode on the regex

--- live-demos/js/regex-tester/index.php
+++ live-demos/js/regex-tester/index.php
@@ -31,7 +31,7 @@
                 var defaultE = false;
                 var defaultC = false;
                     
-                defaultE = '<?php echo escapeRegExp($decodedRegex); ?>';
+                defaultE = escapeRegExp('<?php echo $decodedRegex; ?>');
                 defaultC = '<?php echo $decodedContent; ?>';
                 regX = new RegExer("regexer",defaultE,defaultC);
             };
