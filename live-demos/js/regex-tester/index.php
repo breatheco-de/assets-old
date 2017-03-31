dbc6fd12637235133ebacc2a1025bfe42c723d3b
Alejandro Sanchez <a@stcsolutions.com.ve>
2017-03-30 23:08:59 -0400

    urdecode on the regex

--- live-demos/js/regex-tester/index.php
+++ live-demos/js/regex-tester/index.php
@@ -15,10 +15,10 @@
                 var defaultE = false;
                 var defaultC = false;
                 <?php if(isset($_GET['e'])) { ?>
-                    defaultE = '<?php echo base64_decode(urldecode($_GET["e"])); ?>';
+                    defaultE = '<?php echo base64_decode($_GET["e"]); ?>';
                 <?php } ?>
                 <?php if(isset($_GET['c'])) { ?>
-                    defaultC = '<?php echo base64_decode(urldecode($_GET["c"])); ?>';
+                    defaultC = '<?php echo urldecode($_GET["c"]); ?>';
                 <?php } ?>
                 regX = new RegExer("regexer",defaultE,defaultC);
             };
