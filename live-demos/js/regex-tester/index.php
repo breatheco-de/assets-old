509c86b118251b38e311d5244a2cc2446f7db509
Alejandro Sanchez <a@stcsolutions.com.ve>
2017-03-31 00:20:07 -0400

    urdecode on the regex

--- live-demos/js/regex-tester/index.php
+++ live-demos/js/regex-tester/index.php
@@ -26,7 +26,7 @@
             function stringEscape(s) {
                 return s ? s.replace(/\\/g,'\\\\').replace(/\n/g,'\\n').replace(/\t/g,'\\t').replace(/\v/g,'\\v').replace(/'/g,"\\'").replace(/"/g,'\\"').replace(/[\x00-\x1F\x80-\x9F]/g,hex) : s;
                 function hex(c) { var v = '0'+c.charCodeAt(0).toString(16); return '\\x'+v.substr(v.length-2); }
-            },
+            }
         </script>
         <style type="text/css">
             body{
