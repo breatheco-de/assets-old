3fc5304dd06cb1acfd419373011f082f5273e81d
Alejandro Sanchez <a@stcsolutions.com.ve>
2017-05-22 18:49:48 -0400

    some stuff

--- live-demos/php/forms/http-sniffer.php
+++ live-demos/php/forms/http-sniffer.php
@@ -2,11 +2,49 @@
 <html>
 <head>
 	<title>HTTP Sniffer</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<?php var_dump(getallheaders()); ?>
+	
+	<a href="#" class="button" onClick="toggleAnimation();">OPEN</a>
 
 	<h1>The server received the following:</h1>
-	<?php var_dump($_REQUEST); ?>
+	
+	<div class="email">
+	  <div class="envelope">
+	    <div class="back paper"></div>
+	    <div class="note">
+	      <?php var_dump($_REQUEST); ?>
+	    </div>
+	    <div class="front paper">
+	    	<?php var_dump(getallheaders()); ?>
+	    </div>
+	  </div>
+	</div>
+<script type="text/javascript">
+	function toggleAnimation()
+	{
+		var btns = document.querySelectorAll(".back");
+		var notes = document.querySelectorAll(".note");
+		for(var i =0; i<btns.length;i++)
+		{
+			toggleClass(btns[i],'animate');
+			toggleClass(notes[i],'animate');
+		}
+
+	}
+
+	function toggleClass(elm,className)
+	{
+		if(!elm.classList.contains(className))
+		{
+		  elm.classList.add(className);
+		}
+		else
+		{
+		  elm.classList.remove(className);
+		}
+	}
+</script>
 </body>
 </html>
