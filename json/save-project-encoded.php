9207baa3d43fd48479db9b7b4177a0603adf40e4
Alejandro Sanchez <aalejo@gmail.com>
2017-06-16 19:55:02 +0000

    json APIs

---
+++ json/save-project-encoded.php
@@ -0,0 +1,21 @@
+<?php 
+	header("Content-type: application/json"); 
+	header("Access-Control-Allow-Origin: *");
+
+	$response = [];	
+	try{
+		$response['code'] = 200;	
+		
+		if(count($_POST)==0) throw new Exception('Sad, the project was not saved because the resquest URL is right but you did not specify any form parameters.');
+		if($_SERVER["CONTENT_TYPE"]!='multipart/form-data') throw new Exception('Please set the content-type to form-urlencoded');
+		if(!isset($_POST['id'])) throw new Exception('You need to specify the project id in the parameters of the request');
+		
+		$response['message'] = 'The project with the id '.$_POST['id'].' was successfully saved.';
+	}
+	catch(Exception $e) {
+		$response['code'] = 400;	
+		$response['message'] = $e->getMessage();
+	}
+	
+	echo json_encode($response);
+?>
