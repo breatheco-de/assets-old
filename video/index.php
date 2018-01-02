9ad5c87f353d118cdbdfdabe8ce129054eeda517
Alejandro Sanchez <aalejo@gmail.com>
2018-01-02 16:43:29 +0000

    replit exercises

--- video/index.php
+++ video/index.php
@@ -14,20 +14,20 @@
 	</nav>
 	<!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
     <div id="player"></div>
-	<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+	<script src="/assets/js/jquery-3.2.1.min.js"></script>
 	<script type="text/javascript" src="player.js?v2"></script>
 	<script type="text/javascript">
 		$(document).ready(function(){
-			<?php if(isset($_GET['v'])){ 
-			$autoplay = false;
-			if(isset($_GET['autoplay'])) $autoplay = $_GET['autoplay'];
+			<?php if(isset($_GET['v'])){
+			$autoplay = 'false';
+			if(isset($_GET['autoplay'])) $autoplay = ($_GET['autoplay'] == 'true') ? 'true' : 'false';
 			?>
 			videotutorial.initialize({
 				'selector':'player',
-				'autoplay': <?php echo (bool) $autoplay; ?>,
+				'autoplay': <?php echo $autoplay; ?>,
 				'menu':'dr-menu',
 				'menu-selector':'menu-items',
-				'timeline-url':'video/index/<?php echo $_GET['v']; ?>.json',
+				'timeline-url': '<?php echo $_GET['v']; ?>',
 				'menu-title':'menu-title'
 			});
 			<?php } ?>
