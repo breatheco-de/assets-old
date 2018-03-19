4150149f49d51eee259305be4f5f20a7091d414d
Alejandro Sanchez <aalejo@gmail.com>
2018-03-19 19:56:37 +0000

    meetup api

--- apis/meetup/index.php
+++ apis/meetup/index.php
@@ -2,7 +2,7 @@
     require_once('../APIGenerator.php');
     
 	$api = new APIGenerator('data.json','[]',false);
-	$api->get('meetups', 'Get all survays', function($request, $data){
+	$api->get('meetups', 'Get all calendar events', function($request, $data){
         return $data;
 	});
 	
