ce9a822107081cd8e27c208cf0cf580a1fc98ad7
Jonathan Ferragut <jonnywrites@gmail.com>
2018-10-24 20:46:35 -0400

    update index for new endpoints
    Adding 3 endpoints:
    
    - events
    - meetups
    - session
    
    to match the other pull requests for json data files

--- apis/fake/meetup/index.php
+++ apis/fake/meetup/index.php
@@ -1,9 +1,23 @@
 <?php
     require_once('../../APIGenerator.php');
     
-	$api = new APIGenerator('data.json','[]',false);
-	$api->get('meetups', 'Get all calendar events', function($request, $data){
+	$api_events = new APIGenerator('events.json','[]',false);
+	$api_events->get('events', 'Get all Calendar Events', function($request, $data){
         return $data;
 	});
 	
-	$api->run();
+	$api_events->run();
+
+	$api_meetups = new APIGenerator('meetups.json','[]',false);
+	$api_meetups->get('meetups', 'Get all Meetups', function($request, $data){
+        return $data;
+	});
+	
+	$api_meetups->run();
+
+	$api_session = new APIGenerator('session.json','[]',false);
+	$api_session->get('session', 'Get session', function($request, $data){
+        return $data;
+	});
+	
+	$api_session->run();
