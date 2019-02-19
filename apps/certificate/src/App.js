3344aa3f57a89888d3e4bf8c425b6bb5b3bec7e3
Rafael Esaa <esaarafael@hotmail.com>
2019-02-19 18:00:11 -0400

    test npm run build

--- apps/certificate/src/App.js
+++ apps/certificate/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory'
 
 import ImgIcon from "./component/imgIcon"
 import Notfound from "./component/notFound"
@@ -19,17 +20,25 @@ export default class App extends Component{
       )
     }
 
-    const Img = () => {
+    const Prueba = () => {
       return (
-        <ImgIcon class="mx-auto d-block full-width" data={floridaDepartament}/>
+        <p>Hola!</p>
+      )
+    }
+    
+    const NotFound = () => {
+      return (
+        <p>No hay nada!</p>
       )
     }
 
     return (
-      <Router>
+      <Router basename={'/assets/apps/certificate'}>
         <div>
           <Switch>
             <Route path="/student/:student_id/cohort/:cohort_slug" component={RenderCertificate}/>
+            <Route path="/" component={Prueba}/>
+            <Route path='*' component={NotFound} />
           </Switch>
         </div>
       </Router>
