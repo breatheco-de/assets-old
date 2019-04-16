f1a186c5d37ae8cc6ddf7d3001815c166f3dd99e
Alejandro Sanchez <aalejo@gmail.com>
2019-04-16 15:32:15 +0000

    more configuration

---
+++ globals-example.php
@@ -0,0 +1,55 @@
+<?php
+
+define('version', "1.7");
+
+define('API_DEBUG', true);
+define('JWT_KEY', '');
+define('JWT_CLIENTS', [
+    'nachovz' => "sfdsfdDGFHTrg",
+]);
+
+define('BREATHECODE_HOST', 'https://talenttree-alesanchezr.c9users.io/api/');
+define('BREATHECODE_CLIENT_ID', '');
+define('BREATHECODE_CLIENT_SECRET', '');
+define('BREATHECODE_TOKEN', '');//533887377bc5fc83ef684fc0658a6bef565b2f28
+
+define('ASSETS_HOST', 'https://assets-alesanchezr.c9users.io');
+define('WP_PLATFORM_HOST', 'https://breatheco.de');
+
+define('STUDENT_URL', 'https://bc-js-clients-alesanchezr.c9users.io');
+define('TEACHER_URL', 'https://bc-js-clients-alesanchezr.c9users.io');
+define('ADMIN_URL', 'https://bc-js-clients-alesanchezr.c9users.io');
+
+define('AC_INTEGRATION', false);//execute AC thrid party integrations
+//active campaign api key
+define('AC_API_KEY', '')
+define('AC_EVENT_KEY', '');
+
+//eventbrite
+define('EVENTBRITE_KEY', '');
+
+//slack
+define('SLACK_API_TOKEN', '');
+define('SLACK_API_TOKEN_LEGACY', '');
+
+
+//amazon emails key
+define('ADMIN_EMAIL','');
+define('S3_KEY','');
+define('S3_SECRET','');
+
+define('SQL_LITE_PASSWORD', 'zl3hfu8y');
+
+//upload care
+define('UC_PUBLIC_KEY', '');
+define('UC_SECRET_KEY', '');
+
+//google cloud and places
+define('GOOGLE_PROJECT_ID', '');
+
+define('GOOGLE_PLACES_ID', '');
+define('GOOGLE_PLACES_KEY', '');
+
+//Stream Video Provider
+define('SVP_KEY', "");
+define('SVP_CODE', "");
