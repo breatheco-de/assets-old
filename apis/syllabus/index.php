f9d07f7a87ce34b81b6258c3d56c00af475e7481
Alejandro Sanchez <aalejo@gmail.com>
2019-05-06 19:17:19 +0000

    security for syllabus api

--- apis/syllabus/index.php
+++ apis/syllabus/index.php
@@ -1,12 +1,14 @@
 <?php
 	require_once('../../vendor/autoload.php');
 	require_once('../../globals.php');
-	
+
 	$api = new \SlimAPI\SlimAPI([
 		'name' => 'Replit API - BreatheCode Platform',
-		'debug' => API_DEBUG
+		'debug' => API_DEBUG,
+        'jwt_key' => JWT_KEY,
+        'jwt_clients' => JWT_CLIENTS
 	]);
-	
+
 	$api->addDB('json', new \JsonPDO\JsonPDO('data/','[]',false));
 	$api->addReadme('/','./README.md');
 	$api->addRoutes(require('routes.php'));
