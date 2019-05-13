58d772c12a301f7a1e028d9dd0ca323396c5f0b7
Alejandro Sanchez <aalejo@gmail.com>
2019-05-13 16:52:28 +0000

    vtutorial api now supports asking for a project tutorial

--- apis/vtutorial/index.php
+++ apis/vtutorial/index.php
@@ -4,6 +4,8 @@
 	
 	use Psr\Http\Message\ServerRequestInterface as Request;
 	use Psr\Http\Message\ResponseInterface as Response;
+    use GuzzleHttp\Client;
+
 	$api = new \SlimAPI\SlimAPI([
 		'name' => 'Video Tutorials API - BreatheCode Platform',
 		'debug' => API_DEBUG
@@ -17,6 +19,27 @@
         $content = $api->db['json']->getAllContent();
         
         return $response->withJson($content);
+	});
+    
+	$api->get('/project/{project_slug}', function (Request $request, Response $response, array $args) use ($api) {
+        
+        if(empty($args['project_slug'])) throw new Exception('Invalid param value project_slug');
+
+        $client = new Client();
+
+        $resp = $client->request('GET','https://projects.breatheco.de/json/?slug='.$args['project_slug']);
+
+        if($resp->getStatusCode() != 200) throw new Exception('The project was not found', 404);
+
+        $respText = $resp->getBody()->getContents();
+        $project = json_decode($respText, true);
+        if(empty($project["video-id"])) throw new Exception('The project has no video tutorial', 404);
+
+        
+        return $response->withJson([
+            "video-id" => $project["video-id"],
+            "timeline" => $project["timeline"]
+        ]);
 	});
     
 	$api->get('/{tutorial_slug}', function (Request $request, Response $response, array $args) use ($api) {
