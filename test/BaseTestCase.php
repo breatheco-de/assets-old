0cb3e3739c30624759d951024510c78a75fa4518
Alejandro Sanchez <aalejo@gmail.com>
2019-05-28 19:23:52 +0000

    new test

--- test/BaseTestCase.php
+++ test/BaseTestCase.php
@@ -176,7 +176,8 @@ class BaseTestCase extends TestCase {
 
         foreach($syllabusFiles as $syllabus){
             $profileSlug = pathinfo($syllabus, PATHINFO_FILENAME);
-            $profileSlug = substr($profileSlug, 0, strrpos($profileSlug, ".")); //remove syllabus version
+            $versionNumberPosition = strrpos($profileSlug, ".v");
+            if($versionNumberPosition) $profileSlug = substr($profileSlug, 0, $versionNumberPosition); //remove syllabus version
 
             $this->data['syllabis'][$profileSlug] = json_decode(file_get_contents($syllabus));
             $this->assertSame("Syllabus json for $profileSlug is good", "Syllabus json for $profileSlug is ".($this->data['syllabis'][$profileSlug] ? "good" : "wrong"));
