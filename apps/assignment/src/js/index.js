a50872c03437915bed6d3cd3b9ba51c91b25c217
Alejandro Sanchez <aalejo@gmail.com>
2019-06-24 21:56:49 +0000

    assignments

---
+++ apps/assignment/src/js/index.js
@@ -0,0 +1,12 @@
+//import react into the bundle
+import React from "react";
+import ReactDOM from "react-dom";
+
+//include your index.scss file into the bundle
+import "../styles/index.scss";
+
+//import your own components
+import { Home } from "./home.js";
+
+//render your react application
+ReactDOM.render(<Home />, document.querySelector("#app"));
