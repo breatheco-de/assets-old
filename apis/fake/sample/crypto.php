8ff8bd59e056c0c8f501a4dd28ba79a3f55a7ef2
Alejandro Sanchez <aalejo@gmail.com>
2020-04-29 21:20:17 +0000

    print the project name

--- apis/fake/sample/crypto.php
+++ apis/fake/sample/crypto.php
@@ -1,4 +1,4 @@
 <?php 
 	header("Content-type: application/json"); 
 	header("Access-Control-Allow-Origin: *");
-    echo file_get_contents('https://api.coinmarketcap.com/v1/ticker/bitcoin/');
+    echo file_get_contents('http://api.coindesk.com/v1/bpi/currentprice.json');
