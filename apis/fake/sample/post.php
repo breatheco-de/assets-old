4c56b84ce3c50c00a740ee09de16205a7c6fe60f
Alejandro Sanchez <aalejo@gmail.com>
2020-04-30 04:13:23 +0000

    sample api

--- apis/fake/sample/post.php
+++ apis/fake/sample/post.php
@@ -7,7 +7,6 @@
         $response['code'] = 200;	
         
         if ($_SERVER['REQUEST_METHOD'] != 'POST') throw new Exception('Invalid request method, make sure you are doing a POST request, you seem to be doing a '.$_SERVER['REQUEST_METHOD']);
-		if(strpos($_SERVER["CONTENT_TYPE"],'application/json')==false) throw new Exception('Please set the content-type to application/json, right now is: '.$_SERVER["CONTENT_TYPE"]);
 		
 		$response['message'] = 'Excelent';
 	}
--- apis/fake/sample/save-project-json.php
+++ apis/fake/sample/save-project-json.php
@@ -7,12 +7,13 @@
 		$response['code'] = 200;	
         
         if ($_SERVER['REQUEST_METHOD'] != 'POST') throw new Exception('Invalid request method, make sure you are doing a POST request, you seem to be doing a '.$_SERVER['REQUEST_METHOD']);
-		if(strpos($_SERVER["CONTENT_TYPE"],'json')==false and $_SERVER["CONTENT_TYPE"]!='') throw new Exception('Please set the content-type to application/json, right now is: '.$_SERVER["CONTENT_TYPE"]);
+		if(strpos($_SERVER["CONTENT_TYPE"],'json')==false and $_SERVER["CONTENT_TYPE"]!='') throw new Exception('Please set the content-type to application/json, right now its "'.(empty($_SERVER["CONTENT_TYPE"]) ? "empty" : $_SERVER["CONTENT_TYPE"])."'");
 		$entityBody = file_get_contents('php://input');
 		if(!$entityBody) throw new Exception('The body\'s content is empty, please set the body with a JSON string');
 		
 		$content = json_decode($entityBody);
 		if(!$content) throw new Exception('The body\'s content is not a json or it has some misspells, make sure the format of the body is correct');
+		if(!isset($content->title)) throw new Exception('The project body content is missing a title');
 		
 		$response['message'] = 'The project with the id '.$content->id.' was successfully saved.';
 	}
